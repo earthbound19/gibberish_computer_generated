bc5c678c8fb6d741cc97c49b74b380ae7b24238c
Alex Hall <ansible1@earthbound.io>
2017-02-13 17:14:50 -0700

    POSTMODERN ARTIST STATEMENT DRAWING HAT API V 0.9 displays plain-text postmodern "artistic" nonsense if you pass the php page a "gib" parameter with any value, e.g.: http://earthbound.io/gibberish-artist-statements/index.php?gib=fluouthil -- this do, and you will be rewarded with plain text output something like this:
    ﻿This work rests on the back of our supposed human freedom of speech, and is equally important with the studios of artists engaging in history.

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 
 <?php
 //To sync repo to web server via remote terminal: git.exe pull --progress --no-rebase -v
+// TO DO? Ouptut a proper plain text header from the server if a ?gib value is passed to this script via GET (see comments later)?
+// TO DO? store and print gibberish more elegantly than: echo file_get_contents($gibdir.$source_files_array[$file]);
 
-print("<!doctype html><html lang=\"en\"><head><meta charset=\"utf-8\"\></head><body>");
-print("COMPUTER-GENERATED POSTMODERN \"ARTIST STATEMENT\" DRAWING HAT<br/><br/>May you find the following as meaningful as <a href=\"https://www.canopycanopycanopy.com/contents/international_art_english\">so many other</a> <em>genuine</em> statements. This writing is in the Public Domain; you may use it for any purpose. To get another statement, <a href=\"index.php\">reload the page</a>.<br/><br/>----<br/>");
-
+// SET UP gib.
 // $gibdir = "edited/";
 $gibdir = "International_Art_English_styled_generated_gibberish/pyMarkovGibbGen/edited/";
 // Open a known directory, and read its contents
@@ -23,8 +23,21 @@ if (is_dir($gibdir)) {
     }
 $file = array_rand($source_files_array);
 		// print($gibdir.$source_files_array[$file]);
-echo file_get_contents($gibdir.$source_files_array[$file]);		// get the contents, and echo it out.
-print("<br/>----<br/><br/>You may contribute to these horrors by editing raw output <a href=\"https://github.com/r-alex-hall/gibberish_computer_generated/tree/master/International_Art_English_styled_generated_gibberish/pyMarkovGibbGen/raw_output\">hither</a> and moving it <a href=\"https://github.com/r-alex-hall/gibberish_computer_generated/tree/master/International_Art_English_styled_generated_gibberish/pyMarkovGibbGen/edited\">yon</a> (or by \"fixing up\" anything yon). See the <a href=\"https://github.com/r-alex-hall/gibberish_computer_generated/blob/master/README.md\">README.md</a> file. This \"statement\" is from the source text file <a href=\"International_Art_English_styled_generated_gibberish/pyMarkovGibbGen/edited/" . $source_files_array[$file] . "\"> $source_files_array[$file] </a>.<br/><br/>");
-print("</body></html>");
+
+$gibStr = $_GET["gib"];
+// IF NO "gib" parameter is passed to this script via GET (e.g. ?gib=flarlaugh), print gib with other HTML. If such a parameter is passed, print only plain text gibberish.
+// print("<p>Gibberish string passed to script via GET is: " . $gibStr);
+if ($gibStr != "") {
+  //print("<p>GET value of " . $gibStr . " passed to page.");
+  echo file_get_contents($gibdir.$source_files_array[$file]);		// Print gibberish, in plain text only, with no other HTML or comments accompaniment.
+} else {
+  //print gib with HTML and commentary.
+  print("<!doctype html><html lang=\"en\"><head><meta charset=\"utf-8\"\></head><body>");
+  print("COMPUTER-GENERATED POSTMODERN \"ARTIST STATEMENT\" DRAWING HAT<br/><br/>May you find the following as meaningful as <a href=\"https://www.canopycanopycanopy.com/contents/international_art_english\">so many other</a> <em>genuine</em> statements. This writing is in the Public Domain; you may use it for any purpose. To get another statement, <a href=\"index.php\">reload the page</a>.<br/><br/>----<br/>");
+  echo file_get_contents($gibdir.$source_files_array[$file]);		// get the contents, and echo it out.
+  print("<br/>----<br/><br/>You may contribute to these horrors by editing raw output <a href=\"https://github.com/r-alex-hall/gibberish_computer_generated/tree/master/International_Art_English_styled_generated_gibberish/pyMarkovGibbGen/raw_output\">hither</a> and moving it <a href=\"https://github.com/r-alex-hall/gibberish_computer_generated/tree/master/International_Art_English_styled_generated_gibberish/pyMarkovGibbGen/edited\">yon</a> (or by \"fixing up\" anything yon). See the <a href=\"https://github.com/r-alex-hall/gibberish_computer_generated/blob/master/README.md\">README.md</a> file. This \"statement\" is from the source text file <a href=\"International_Art_English_styled_generated_gibberish/pyMarkovGibbGen/edited/" . $source_files_array[$file] . "\"> $source_files_array[$file] </a>.<br/><br/>");
+  print("</body></html>");
+  }
+
 }
 ?>
